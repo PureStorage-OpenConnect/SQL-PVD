26fe32d68c734d16cd613c21cfa8edf75d94564b
Mike Nelson <mike@mikenelson.io>
2021-02-16 11:24:41 -0600

    needs code

---
+++ scripts/Create-ActiveCluster.ps1
@@ -0,0 +1,18 @@
+# Create-ActiveCluster.ps1
+#
+# : Revision 1.0.0.0
+# :: initial release
+#
+# Example script to create a FlashArray ActiveCluster configuration.
+# This is not intended to be a complete run script. It is for example purposes only.
+# Variables should be modified to suit the environment.
+#
+# This script is AS-IS. No warranties expressed or implied by Pure Storage or the author.
+#
+# Requirements:
+#  Pure Storage PowerShell SDK v1 module
+#  Flasharray array admin login credentials
+#
+#
+#### Start
+## Define variables
